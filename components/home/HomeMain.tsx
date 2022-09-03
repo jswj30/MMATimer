6b783d4ccf5fc38ea9a18e79ba006c1e9c47d66e
jswj <jswj30@naver.com>
2022-09-03 09:15:38 +0900

    1

--- components/home/HomeMain.tsx
+++ components/home/HomeMain.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
 
 const HomeMain = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <Text>Home</Text>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+  },
+});
 
 export default HomeMain;
