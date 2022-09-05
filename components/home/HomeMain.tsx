3130e503d00576d6fdd39a71d2cb7024995d7a59
jswj <jswj30@naver.com>
2022-09-05 21:44:29 +0900

    stopwatch test

--- components/home/HomeMain.tsx
+++ components/home/HomeMain.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
-import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text, SafeAreaView, StyleSheet, Pressable} from 'react-native';
 
 const HomeMain = () => {
+  const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(false);
+
+  useEffect(() => {
+    let interval;
+    if (running) {
+      interval = setInterval(() => {
+        setTime(prevTime => prevTime + 10);
+      }, 10);
+    } else if (!running) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [running]);
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Home</Text>
+      <Text style={{fontSize: 30}}>
+        {('0' + Math.floor((time / 60000) % 60)).slice(-2)}:
+        {('0' + Math.floor((time / 1000) % 60)).slice(-2)}:
+        {('0' + ((time / 10) % 100)).slice(-2)}
+      </Text>
+      <Text></Text>
+      <Pressable onPress={() => setRunning(true)}>
+        <Text>Start</Text>
+      </Pressable>
+      <Pressable onPress={() => setRunning(false)}>
+        <Text>Stop</Text>
+      </Pressable>
+      <Pressable onPress={() => setTime(0)}>
+        <Text>Reset</Text>
+      </Pressable>
     </SafeAreaView>
   );
 };
@@ -13,6 +42,9 @@ const styles = StyleSheet.create({
   container: {
     width: '100%',
     height: '100%',
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
